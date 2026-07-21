5097667328ccdf2619281cba4701782b97548833
komuhana1216 <galleria0302@gmail.com>
2026-07-21 10:20:18 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
-const buttons = document.querySelectorAll(".btn");
+// ボタンを順番に表示
+document.querySelectorAll(".btn").forEach((btn,index)=>{
 
-buttons.forEach((button,index)=>{
+btn.style.opacity=0;
+btn.style.transform="translateY(25px)";
 
-    button.style.opacity="0";
-    button.style.transform="translateY(30px)";
+setTimeout(()=>{
 
-    setTimeout(()=>{
+btn.style.transition=".7s";
 
-        button.style.transition=".6s";
+btn.style.opacity=1;
+btn.style.transform="translateY(0px)";
 
-        button.style.opacity="1";
-        button.style.transform="translateY(0)";
+},index*180);
 
-    },250*index);
+});
+
+// カードを少し動かす
+document.addEventListener("mousemove",(e)=>{
+
+const x=(window.innerWidth/2-e.clientX)/50;
+const y=(window.innerHeight/2-e.clientY)/50;
+
+document.querySelector(".container").style.transform=
+`rotateY(${x}deg) rotateX(${-y}deg)`;
 
 });
